16416e58f7b22aad4f87342e07478ae8ccbe8771
Qid-20006832 <20006832@rp.edu.sg>
2021-07-14 17:12:33 +0800

    Allocated parts for each member

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -3,6 +3,12 @@ public class C206_CaseStudy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//1-dan-useraccount
+		//2-jh-category
+		//3-nelly-item
+		//4-qid-bid
+		//5-gy-deal
 
 	}
 
