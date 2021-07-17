01a8c2d6c3e4e013c75528cf6170d34a89050a5d
20006832 <20006832@LAPTOP-L9GNROPG.mshome.net>
2021-07-17 19:31:28 +0800

    create commit

---
+++ C206_CaseStudy/src/item.java
@@ -0,0 +1,18 @@
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 20006832, 17 Jul 2021 7:29:52 pm
+ */
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 20006832, 17 Jul 2021 7:29:52 pm
+ */
+public class item {
+
+}
