cafc4c0de9b70fe4776386552202d4aa156d873b
20016936 <20016936@LAPTOP-LNM8NQ6U>
2021-07-31 13:03:32 +0800

    Created main class for the Sprint. Use this main class instead of C206_CaseStudy.Java. It will be deleted. Please update the changes on your individual assignment.

---
+++ C206_CaseStudy/src/COAS.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class COAS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayList<item> accountlist = new ArrayList<item>();
+		
+		//1-dan-useraccount
+		//2-jh-category
+		//3-nelly-item
+		//hello i was here
+		//4-qid-bid
+		//5-gy-deal
+	}
+
+}
