7cee32927008cd33c1cd79f85225a5bee1320e74
20016936 <20016936@LAPTOP-LNM8NQ6U>
2021-07-31 16:04:28 +0800

    test

--- C206_CaseStudy/src/COAS.java
+++ C206_CaseStudy/src/COAS.java
@@ -1,11 +1,10 @@
 import java.util.ArrayList;
 
 public class COAS {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int option = 0;
-		
 		while (option!=4) {
 			
 			viewMenu();
@@ -99,10 +98,7 @@ public class COAS {
 		
 		
 		//1-dan-useraccount
-		ArrayList<account> accountlist = new ArrayList<account>();
 		//test
-		
-		
 		
 		
 		
