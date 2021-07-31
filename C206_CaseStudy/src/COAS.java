947c888d58a5b4b6b0955158f3a109b730d05813
20016936 <20016936@LAPTOP-LNM8NQ6U>
2021-07-31 13:42:09 +0800

    Basic menu option created.

--- C206_CaseStudy/src/COAS.java
+++ C206_CaseStudy/src/COAS.java
@@ -4,7 +4,25 @@ public class COAS {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int option = 0;
+		while (option!=4) {
+			viewMenu();
+			option = Helper.readInt("Enter option > ");
+			
+			if (option == 1) {
+				
+			} else if (option == 2){
+				
+			} else if (option == 3) {
+				
+			} else if (option == 4) {
+				System.out.println("Thank for using the application!");
+				option = 4;
+			} else {
+				System.out.println("Invalid Option!");
+			}
+		}
+		
 		
 		//1-dan-useraccount
 		ArrayList<account> accountlist = new ArrayList<account>();
@@ -42,6 +60,17 @@ public class COAS {
 		
 		
 		//5-gy-deal
+	}
+	
+	public static void viewMenu() {
+		Helper.line(35, "-");
+		System.out.println("CAMPUS Online Auction Shop (COAS)");
+		Helper.line(35, "-");
+		System.out.println("1. ADD");
+		System.out.println("2. VIEW");
+		System.out.println("3. DELETE");
+		System.out.println("4. QUIT");
+		Helper.line(35, "-");
 	}
 
 }
--- C206_CaseStudy/src/account.java
+++ C206_CaseStudy/src/account.java
@@ -1,3 +1,10 @@
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 20016936 Daniel Ko, 31 Jul 2021 1:25:32 pm
+ */
 
 public class account {
 	
